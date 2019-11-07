4fab5f39700d3ab41d442dfa9fb4baf232baad66
sarahduv <sarah.duvdevani@gmail.com>
2019-11-06 17:34:13 -0800

    Test and lint are passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,19 +7,40 @@ const events = require('../src/modules/events.js');
 require('../src/modules/read-file.js');
 require('../src/modules/write-file.js');
 require('../src/modules/uppercase.js');
-require('../src/modules/app-server-client.js');
+const serverClient = require('../src/modules/app-server-client.js');
+
+const mockedClient = {
+  connected: false,
+  on_calls: [],
+  write_calls: [],
+  connect() { this.connected = true; },
+  destroy() { this.connected = false; },
+  write(payload) { this.write_calls.push(payload); },
+  on(name, callback) { this.on_calls.push({name, callback}); },
+};
+serverClient(mockedClient);
 
 
 jest.mock('fs');
 
 describe('Event application tests', () => {
 
-  it('can ', () => {
-    const spy = sinon.spy();
-    const emitter = new EventEmitter();
-    emitter.on('read_done', spy);
-    emitter.emit('read_done');
-    expect(spy.called).toBe(true);
+  beforeEach(async () => {
+    mockedClient.connected = false;
+    mockedClient.on_calls = [];
+    mockedClient.write_calls = [];
+  });
+
+  it('can broadcast save', async () => {
+    await events.emit('write_done', 'SUCCESS');
+    expect(mockedClient.write_calls.length).toBe(1);
+    expect(JSON.parse(mockedClient.write_calls[0]).payload).toEqual('SUCCESS');
+  });
+
+  it('can broadcast error', async () => {
+    await events.emit('read_error', 'failed!');
+    expect(mockedClient.write_calls.length).toBe(1);
+    expect(JSON.parse(mockedClient.write_calls[0]).payload).toEqual('failed!');
   });
 
   it('can log a file', () => {
